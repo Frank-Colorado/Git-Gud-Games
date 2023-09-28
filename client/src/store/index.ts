8d93a108c0366c25236bd1c4feb53f4f8521c6d0
Frank-Colorado <chawnkiee@gmail.com>
2023-09-28 15:02:23 -0400

    configured store in store folder

---
+++ client/src/store/index.ts
@@ -0,0 +1,19 @@
+import { configureStore } from '@reduxjs/toolkit';
+import { setupListeners } from '@reduxjs/toolkit/query';
+import { gamesApi } from './apis/gamesApi';
+
+export const store = configureStore({
+  reducer: {
+    [gamesApi.reducerPath]: gamesApi.reducer,
+  },
+  middleware: (getDefaultMiddleware) => {
+    return getDefaultMiddleware().concat(gamesApi.middleware);
+  },
+});
+
+setupListeners(store.dispatch);
+
+export {
+  useGetGamesByGenreQuery,
+  useGetGamesBySearchTermQuery,
+} from './apis/gamesApi';

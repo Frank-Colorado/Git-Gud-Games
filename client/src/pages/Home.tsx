fa2472f1715a20097d16347e8c05cee983bd22d7
Frank-Colorado <chawnkiee@gmail.com>
2023-09-29 14:35:21 -0400

    removed code from Home.tsx file for refactor

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,8 +1,5 @@
 import { useParams } from 'react-router-dom';
-import Grid from '@mui/material/Grid';
-import { Typography } from '@mui/material';
-import Sidebar from '../components/Sidebar';
-import GamesDisplay from '../components/GamesDisplay';
+
 import { GameApiQuery } from '../store';
 
 interface HomeProps {
@@ -11,17 +8,9 @@ interface HomeProps {
 
 const Home = ({ gamesQuery }: HomeProps) => {
   return (
-    <Grid container sx={{ height: '100vh' }}>
-      <Grid item md={2} sx={{ backgroundColor: 'green' }}>
-        <Sidebar />
-      </Grid>
-      <Grid item md={10} sx={{ backgroundColor: 'blue' }}>
-        <Typography variant="h2" sx={{ mb: 2, color: 'white' }} align="center">
-          Here's What's Good...
-        </Typography>
-        <GamesDisplay gamesQuery={gamesQuery} />
-      </Grid>
-    </Grid>
+    <div>
+      <h1>placeholder</h1>
+    </div>
   );
 };
 

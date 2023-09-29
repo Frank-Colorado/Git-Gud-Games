ec1dadbb5acb49e9b90b174c1596a6c307c829e1
Frank-Colorado <chawnkiee@gmail.com>
2023-09-29 15:09:21 -0400

    created GenrePage component

--- client/src/pages/GenrePage.tsx
+++ client/src/pages/GenrePage.tsx
@@ -1,8 +1,15 @@
+import { useParams } from 'react-router-dom';
+import MainDisplay from '../components/MainDisplay';
+
 const GenrePage = () => {
+  const { genre } = useParams();
+  const headerText = `{genre} Games`;
+  const queryText = `&genres=${genre}`;
   return (
     <div>
-      <h1>Genre Page</h1>
+      <MainDisplay header={headerText} query={queryText} />
     </div>
   );
 };
+
 export default GenrePage;

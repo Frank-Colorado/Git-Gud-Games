bba2dd6c189cd2ce9fb1577089c0b42fec2440e6
Frank-Colorado <chawnkiee@gmail.com>
2023-09-29 20:30:11 -0400

    added code to Autocomplete component

--- client/src/components/Autocomplete.tsx
+++ client/src/components/Autocomplete.tsx
@@ -1,8 +1,39 @@
+import { useState } from 'react';
+import {
+  TextField,
+  Autocomplete as MuiAutocomplete,
+  Typography,
+} from '@mui/material';
+import { Game } from '../store';
+
 const Autocomplete = () => {
+  const [value, setValue] = useState<Game | null>(null);
+  const [inputValue, setInputValue] = useState('');
+  const [options, setOptions] = useState<Game[]>([]);
   return (
-    <div>
-      <h1> Autocomplete</h1>
-    </div>
+    <MuiAutocomplete
+      disablePortal
+      sx={{ width: 300 }}
+      options={options}
+      getOptionLabel={(option) =>
+        typeof option === 'string' ? option : option.name
+      }
+      filterOptions={(x) => x}
+      autoComplete
+      includeInputInList
+      value={value}
+      noOptionsText={<Typography>No results found</Typography>}
+      onChange={(event, newValue: Game | null) => {
+        setOptions(newValue ? [newValue, ...options] : options);
+        setValue(newValue);
+      }}
+      onInputChange={(event, newInputValue) => {
+        setInputValue(newInputValue);
+      }}
+      renderInput={(params) => (
+        <TextField {...params} label="Combo box" placeholder="Favorites" />
+      )}
+    />
   );
 };
 

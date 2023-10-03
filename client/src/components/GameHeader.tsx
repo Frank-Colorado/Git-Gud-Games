2de222c6ff55f379df067a999f49c34b0272090d
Frank-Colorado <chawnkiee@gmail.com>
2023-10-03 19:41:39 -0400

    added code to GameHeader component

--- client/src/components/GameHeader.tsx
+++ client/src/components/GameHeader.tsx
@@ -1,8 +1,31 @@
-const GameHeader = () => {
+import { Box, Typography, Button } from '@mui/material';
+import { GameDetailsType } from '../store/apis/gamesApi';
+
+interface GameHeaderProps {
+  data: GameDetailsType;
+}
+
+const GameHeader = ({ data }: GameHeaderProps) => {
+  const handleClick = () => {
+    // Game will be added to the user's library if they are logged in
+    console.log('Add to Library Clicked', data.id);
+  };
+
   return (
-    <div>
-      <h1>Game Header</h1>
-    </div>
+    <Box
+      sx={{
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+      }}
+    >
+      <Typography variant="h3" sx={{ color: 'white' }}>
+        {data.name}
+      </Typography>
+      <Button variant="contained" color="primary" onClick={handleClick}>
+        Add to Library
+      </Button>
+    </Box>
   );
 };
 

3858b8921ba673a64c5cd3a4e7085fe01ed38701
Frank-Colorado <chawnkiee@gmail.com>
2023-10-04 19:09:18 -0400

    created server boilerplate

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,13 @@
-console.log('Hello world!');
+import express, { Express } from 'express';
+
+const app: Express = express();
+
+const PORT: string | number = process.env.PORT || 3001;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});

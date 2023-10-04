48189d6391c986102437e2f4826b8cb609a6867f
Frank-Colorado <chawnkiee@gmail.com>
2023-10-04 19:25:46 -0400

    set up apollo server in server/index.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,13 +1,28 @@
 import express, { Express } from 'express';
+import { ApolloServer } from '@apollo/server';
+import { buildSchema } from 'type-graphql';
 
-const app: Express = express();
+const main = async () => {
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [],
+      validate: false,
+    }),
+  });
 
-const PORT: string | number = process.env.PORT || 3001;
+  const app: Express = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+  const PORT: string | number = process.env.PORT || 3001;
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  app.get('/', (req, res) => {
+    res.send('Hello World!');
+  });
+
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+};
+
+main().catch((err) => {
+  console.error(err);
 });

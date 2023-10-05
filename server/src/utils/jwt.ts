2d2ced8ac0905bc5dded8bc591ee23cbce78a15d
Frank-Colorado <chawnkiee@gmail.com>
2023-10-05 19:57:21 -0400

    created customEnvVariables file in config

--- server/src/utils/jwt.ts
+++ server/src/utils/jwt.ts
@@ -1,29 +1,6 @@
-import dotenv from 'dotenv';
-dotenv.config();
-import jwt from 'jsonwebtoken';
+import config from 'config';
 
-const publicKey = process.env.JWT_PUBLIC_KEY;
-const privateKey = process.env.JWT_PRIVATE_KEY;
-
-const publicKeyBuffer = Buffer.from(publicKey, 'base64').toString('ascii');
-const privateKeyBuffer = Buffer.from(privateKey!, 'base64').toString('ascii');
-
-console.log(publicKeyBuffer);
-console.log(privateKeyBuffer);
+const publicKey = config.get('publicKey');
 
 export const signJwt = () => {};
-
-// export const signJwt = (
-//   object: Object,
-//   options?: jwt.SignOptions | undefined
-// ) => {
-//   const payload = {
-//     ...object,
-//   };
-//   return jwt.sign({ data: payload }, secret, {
-//     ...(options && options),
-//     algorithm: 'RS256',
-//   });
-// };
-
-// export const verifyJwt = (token) => {};
+export const verifyJwt = () => {};

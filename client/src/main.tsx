0a4d462cf41600a6aade49c1523a61673ffa97df
Frank-Colorado <chawnkiee@gmail.com>
2023-10-05 23:56:46 -0400

    setup Apollo Client in client/main.tsx file

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,9 +2,38 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import { store } from './store';
 import { Provider } from 'react-redux';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('jwtToken');
+
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <Provider store={store}>
-    <App />
-  </Provider>
+  <ApolloProvider client={client}>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </ApolloProvider>
 );

c2032c44232aa99fda274927acf9f31ffb87b813
Frank-Colorado <chawnkiee@gmail.com>
2023-10-06 18:51:23 -0400

    added some styling to LoginPage

--- client/src/pages/LoginPage.tsx
+++ client/src/pages/LoginPage.tsx
@@ -6,58 +6,81 @@ import {
   Button,
   FormControl,
 } from '@mui/material';
-import { alpha, styled } from '@mui/material/styles';
+import { styled } from '@mui/material/styles';
+import { Link } from 'react-router-dom';
+
+const StyledTextField = styled(TextField)({
+  '& label.Mui-focused': {
+    color: '#EE9322',
+  },
+  '& .MuiInput-underline:after': {
+    borderBottomColor: '#EE9322',
+  },
+  '& .MuiOutlinedInput-root': {
+    '& fieldset': {
+      borderColor: '#EE9322',
+    },
+    '&:hover fieldset': {
+      borderColor: '#EE9322',
+    },
+    '&.Mui-focused fieldset': {
+      borderColor: '#EE9322',
+    },
+  },
+});
 
 const LoginPage = () => {
   return (
     <Grid
       container
       sx={{
-        height: '100vh',
+        height: 700,
         display: 'flex',
+        flexDirection: 'column',
         justifyContent: 'center',
-        marginTop: '5rem',
-        marginBottom: '5rem',
+        alignItems: 'center',
       }}
     >
-      <Grid
-        item
-        md={8}
-        sx={{
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}
-      >
-        <Typography variant="h1">Login Page</Typography>
-        <Box sx={{ mt: 10 }}>
-          <form
-            style={{
-              display: 'flex',
-              flexDirection: 'column',
-              alignItems: 'center',
-              width: '500px',
+      <Typography variant="h1" sx={{ color: '#D83F31' }}>
+        Login
+      </Typography>
+      <Box sx={{ mt: 10, border: '1px solid #D83F31 ' }}>
+        <form
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            width: '500px',
+            color: 'white',
+            padding: '2rem',
+          }}
+        >
+          <StyledTextField
+            label="Username"
+            sx={{ marginBottom: '3rem' }}
+            InputLabelProps={{
+              sx: {
+                color: '#EE9322',
+              },
             }}
-          >
-            <TextField
-              id="outlined-basic"
-              label="Username"
-              variant="filled"
-              sx={{ marginBottom: '4rem', color: 'white' }}
-            />
-            <TextField
-              id="outlined-basic"
-              label="Password"
-              variant="outlined"
-              sx={{ marginBottom: '4rem' }}
-            />
-            <Button variant="contained" type="submit">
-              Login
-            </Button>
-          </form>
-        </Box>
-      </Grid>
+          />
+          <StyledTextField
+            label="Password"
+            sx={{ marginBottom: '3rem' }}
+            InputLabelProps={{
+              sx: {
+                color: '#EE9322',
+              },
+            }}
+          />
+          <Button variant="contained" type="submit">
+            Login
+          </Button>
+          <Typography variant="subtitle1" sx={{ marginTop: '1rem' }}>
+            Don't have an account? Sign up <Link to="/signup">here</Link> !
+          </Typography>
+        </form>
+      </Box>
     </Grid>
   );
 };

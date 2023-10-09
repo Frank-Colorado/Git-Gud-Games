15bac0f0c96a2e75a049593fe1ccc0bf8ac2ae93
Frank-Colorado <chawnkiee@gmail.com>
2023-10-09 17:04:56 -0400

    deleted thunks folder

--- client/src/store/thunks/fetchUser.ts
+++
@@ -1,9 +0,0 @@
-import { createAsyncThunk } from '@reduxjs/toolkit';
-import { apolloClient } from '../../graphql/apolloClient';
-import { GET_ME } from '../../graphql/queries';
-
-export const fetchUser = createAsyncThunk('user/fetch', async () => {
-  const response = useQuery(GET_ME);
-
-  return response.data;
-});

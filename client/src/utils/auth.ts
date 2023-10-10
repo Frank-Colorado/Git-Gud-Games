9bdaba94980c684a3d36ccd6ec7fd5afb8cd48cc
Frank-Colorado <chawnkiee@gmail.com>
2023-10-09 20:12:01 -0400

    created AuthService class in auth.ts

---
+++ client/src/utils/auth.ts
@@ -0,0 +1,35 @@
+import decode from 'jwt-decode';
+
+class AuthService {
+  loggedIn() {
+    const token = this.getToken();
+    return !!token && !this.isTokenExpired(token);
+  }
+
+  isTokenExpired(token: string) {
+    try {
+      const decoded = decode(token) as any;
+      if (decoded.exp < Date.now() / 1000) {
+        return true;
+      } else return false;
+    } catch (err) {
+      return false;
+    }
+  }
+
+  getToken() {
+    return localStorage.getItem('jwtToken');
+  }
+
+  login(idToken: string) {
+    localStorage.setItem('jwtToken', idToken);
+    window.location.assign('/');
+  }
+
+  logout() {
+    localStorage.removeItem('jwtToken');
+    window.location.reload();
+  }
+}
+
+export default new AuthService();

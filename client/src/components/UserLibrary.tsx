010ab732acd6297f2e15c08e6fd9db623cae0429
Frank-Colorado <chawnkiee@gmail.com>
2023-10-11 19:57:25 -0400

    moved code to UserLibrary component

--- client/src/components/UserLibrary.tsx
+++ client/src/components/UserLibrary.tsx
@@ -1,8 +1,90 @@
+import { Box, Grid, Typography } from '@mui/material';
+
 const UserLibrary = () => {
   return (
-    <div>
-      <h1>User Library</h1>
-    </div>
+    <>
+      <Box sx={{ height: 100, marginLeft: 2 }}>
+        <Typography variant="h2">My Library</Typography>
+      </Box>
+      <Grid container spacing={2} sx={{ paddingX: 2 }}>
+        <Grid item xs={6}>
+          <Box
+            sx={{
+              bgcolor: 'purple',
+              display: 'flex',
+              alignItems: 'center',
+              height: 150,
+            }}
+          >
+            <img
+              style={{ padding: '2rem' }}
+              src="https://picsum.photos/175/125"
+              alt="logo"
+            />
+            <Typography variant="h5" sx={{ color: 'white' }}>
+              Game Title
+            </Typography>
+          </Box>
+        </Grid>
+        <Grid item xs={6}>
+          <Box
+            sx={{
+              bgcolor: 'purple',
+              display: 'flex',
+              alignItems: 'center',
+              height: 150,
+            }}
+          >
+            <img
+              style={{ padding: '2rem' }}
+              src="https://picsum.photos/175/125"
+              alt="logo"
+            />
+            <Typography variant="h5" sx={{ color: 'white' }}>
+              Game Title
+            </Typography>
+          </Box>
+        </Grid>
+        <Grid item xs={6}>
+          <Box
+            sx={{
+              bgcolor: 'purple',
+              display: 'flex',
+              alignItems: 'center',
+              height: 150,
+            }}
+          >
+            <img
+              style={{ padding: '2rem' }}
+              src="https://picsum.photos/175/125"
+              alt="logo"
+            />
+            <Typography variant="h5" sx={{ color: 'white' }}>
+              Game Title
+            </Typography>
+          </Box>
+        </Grid>
+        <Grid item xs={6}>
+          <Box
+            sx={{
+              bgcolor: 'purple',
+              display: 'flex',
+              alignItems: 'center',
+              height: 150,
+            }}
+          >
+            <img
+              style={{ padding: '2rem' }}
+              src="https://picsum.photos/175/125"
+              alt="logo"
+            />
+            <Typography variant="h5" sx={{ color: 'white' }}>
+              Game Title
+            </Typography>
+          </Box>
+        </Grid>
+      </Grid>
+    </>
   );
 };
 

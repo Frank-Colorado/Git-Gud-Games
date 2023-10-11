4be975f06fafb06629221c4d06c8c4e5757c4239
Frank-Colorado <chawnkiee@gmail.com>
2023-10-11 19:53:59 -0400

    transfered header code to UserHeader comp

--- client/src/components/UserHeader.tsx
+++ client/src/components/UserHeader.tsx
@@ -1,7 +1,52 @@
+import { Box, Button, Typography } from '@mui/material';
+
 const UserHeader = () => {
   return (
     <div>
-      <h1>User Header</h1>
+      <Box
+        sx={{
+          width: '100%',
+          display: 'flex',
+          justifyContent: 'end',
+          bgcolor: 'yellow',
+        }}
+      >
+        <Button variant="contained">Edit Profile</Button>
+      </Box>
+      <Box
+        sx={{
+          bgcolor: 'orange',
+          overflow: 'hidden',
+          height: '20rem',
+
+          display: 'flex',
+          alignItems: 'center',
+        }}
+      >
+        <Box
+          sx={{
+            marginLeft: '3rem',
+          }}
+        >
+          <img src="https://picsum.photos/300" alt="logo" />
+        </Box>
+        <Box
+          sx={{
+            margin: '2rem',
+            bgcolor: 'green',
+            height: '95%',
+            width: '100%',
+          }}
+        >
+          <Typography variant="h3">Username</Typography>
+          <Typography variant="body1" sx={{ marginTop: '2rem' }}>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eum,
+            deserunt delectus suscipit officia nesciunt, pariatur nulla incidunt
+            voluptatibus animi expedita, sunt soluta autem quae ea facilis
+            perspiciatis! Similique, perspiciatis quibusdam.
+          </Typography>
+        </Box>
+      </Box>
     </div>
   );
 };
--- client/src/pages/UserPage.tsx
+++ client/src/pages/UserPage.tsx
@@ -10,50 +10,6 @@ const UserPage = () => {
       }}
     >
       <Grid item md={7} sx={{ bgcolor: 'red' }}>
-        <Box
-          sx={{
-            width: '100%',
-            display: 'flex',
-            justifyContent: 'end',
-            bgcolor: 'yellow',
-          }}
-        >
-          <Button variant="contained">Edit Profile</Button>
-        </Box>
-        <Box
-          sx={{
-            bgcolor: 'orange',
-            overflow: 'hidden',
-            height: '20rem',
-
-            display: 'flex',
-            alignItems: 'center',
-          }}
-        >
-          <Box
-            sx={{
-              marginLeft: '3rem',
-            }}
-          >
-            <img src="https://picsum.photos/300" alt="logo" />
-          </Box>
-          <Box
-            sx={{
-              margin: '2rem',
-              bgcolor: 'green',
-              height: '95%',
-              width: '100%',
-            }}
-          >
-            <Typography variant="h3">Username</Typography>
-            <Typography variant="body1" sx={{ marginTop: '2rem' }}>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eum,
-              deserunt delectus suscipit officia nesciunt, pariatur nulla
-              incidunt voluptatibus animi expedita, sunt soluta autem quae ea
-              facilis perspiciatis! Similique, perspiciatis quibusdam.
-            </Typography>
-          </Box>
-        </Box>
         <Box sx={{ height: 100, marginLeft: 2 }}>
           <Typography variant="h2">My Library</Typography>
         </Box>

f177ecc076237f55e0474c369f224ef736b7585b
Frank-Colorado <chawnkiee@gmail.com>
2023-10-11 21:43:58 -0400

    added code to load games from state in UserLibrary

--- client/src/components/UserLibrary.tsx
+++ client/src/components/UserLibrary.tsx
@@ -1,7 +1,9 @@
+import { useAppSelector } from '../hooks';
 import { Box, Grid, Typography } from '@mui/material';
 import LibraryItem from './LibraryItem';
 
 const UserLibrary = () => {
+  const { user } = useAppSelector((state) => state.user);
   return (
     <>
       <Box sx={{ height: 100, marginLeft: 2 }}>
@@ -9,14 +11,19 @@ const UserLibrary = () => {
           My Library
         </Typography>
       </Box>
-      <Grid container spacing={2} sx={{ paddingX: 2 }}>
-        <LibraryItem />
-        <LibraryItem />
-        <LibraryItem />
-        <LibraryItem />
-        <LibraryItem />
-        <LibraryItem />
-      </Grid>
+      {user.gameLibrary.length > 0 ? (
+        <Grid container spacing={2} sx={{ paddingX: 2 }}>
+          {user.gameLibrary.map((game) => (
+            <div key={game.id}>
+              <LibraryItem game={game} />
+            </div>
+          ))}
+        </Grid>
+      ) : (
+        <Typography variant="h5" textAlign="center" sx={{ color: 'white' }}>
+          You don't have any games in your library yet!
+        </Typography>
+      )}
     </>
   );
 };

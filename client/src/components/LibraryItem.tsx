638d740b3354ffa0497aa044a150b897465750e2
Frank-Colorado <chawnkiee@gmail.com>
2023-10-11 20:10:10 -0400

    moved code to LibraryItem component

--- client/src/components/LibraryItem.tsx
+++ client/src/components/LibraryItem.tsx
@@ -1,5 +1,29 @@
+import { Box, Grid, Typography } from '@mui/material';
+
 const LibraryItem = () => {
-  return <div>LibraryItem</div>;
+  return (
+    <>
+      <Grid item xs={6}>
+        <Box
+          sx={{
+            bgcolor: 'purple',
+            display: 'flex',
+            alignItems: 'center',
+            height: 150,
+          }}
+        >
+          <img
+            style={{ padding: '2rem' }}
+            src="https://picsum.photos/175/125"
+            alt="logo"
+          />
+          <Typography variant="h5" sx={{ color: 'white' }}>
+            Game Title
+          </Typography>
+        </Box>
+      </Grid>
+    </>
+  );
 };
 
 export default LibraryItem;

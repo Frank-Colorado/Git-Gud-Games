bccacbadb514358915f0abc457b233ad0f1ecc74
Frank-Colorado <chawnkiee@gmail.com>
2023-10-11 21:47:09 -0400

    refactored LibraryItem comp to load game data

--- client/src/components/LibraryItem.tsx
+++ client/src/components/LibraryItem.tsx
@@ -1,6 +1,15 @@
 import { Box, Grid, Typography } from '@mui/material';
+import { Link } from 'react-router-dom';
 
-const LibraryItem = () => {
+interface LibraryItemProps {
+  game: {
+    id: string;
+    name: string;
+    image: string;
+  };
+}
+
+const LibraryItem = ({ game }: LibraryItemProps) => {
   return (
     <>
       <Grid item xs={6}>
@@ -12,14 +21,12 @@ const LibraryItem = () => {
             height: 150,
           }}
         >
-          <img
-            style={{ padding: '2rem' }}
-            src="https://picsum.photos/175/125"
-            alt="logo"
-          />
-          <Typography variant="h5" sx={{ color: 'white' }}>
-            Game Title
-          </Typography>
+          <img style={{ padding: '2rem' }} src={game.image} alt="logo" />
+          <Link to="/game/${game.id}" style={{ textDecoration: 'none' }}>
+            <Typography variant="h5" sx={{ color: 'white' }}>
+              {game.name}
+            </Typography>
+          </Link>
         </Box>
       </Grid>
     </>

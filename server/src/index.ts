64dfb1b6deeae2202c1cbdf7a43237a4936ee2f9
Frank-Colorado <chawnkiee@gmail.com>
2023-10-20 19:23:24 -0400

    server code edits for vite production

--- server/src/index.ts
+++ server/src/index.ts
@@ -50,12 +50,12 @@ const main = async () => {
   app.use(express.urlencoded({ extended: false }));
 
   if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/build')));
-  }
+    app.use(express.static(path.join(__dirname, 'dist')));
 
-  app.get('/', (req: Request, res: Response) => {
-    res.sendFile(path.join(__dirname, './client/build/index.html'));
-  });
+    app.get('/', (req: Request, res: Response) => {
+      res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+    });
+  }
 
   await server.start();
 

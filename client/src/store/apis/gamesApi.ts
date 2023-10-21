43390e660fe2cf1bc1232c30d6c7381e7384eddd
Frank-Colorado <chawnkiee@gmail.com>
2023-10-20 20:10:54 -0400

    changes to gamesApi file

--- client/src/store/apis/gamesApi.ts
+++ client/src/store/apis/gamesApi.ts
@@ -1,4 +1,6 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import dotenv from 'dotenv';
+dotenv.config();
 const apiKey = process.env.VITE_RAWG_API_KEY;
 const baseUrl = `https://api.rawg.io/api`;
 
--- server/src/index.ts
+++ server/src/index.ts
@@ -51,11 +51,11 @@ const main = async () => {
 
   if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../../client/dist')));
-
-    app.get('/', (req: Request, res: Response) => {
-      res.sendFile(path.join(__dirname, '../../client/dist', 'index.html'));
-    });
   }
+
+  app.get('/', (req: Request, res: Response) => {
+    res.sendFile(path.join(__dirname, '../../client/dist', 'index.html'));
+  });
 
   await server.start();
 

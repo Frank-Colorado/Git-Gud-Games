54c3dcaf44db35d13bc6a7b00471dfe966cde229
Frank-Colorado <chawnkiee@gmail.com>
2023-10-20 20:24:59 -0400

    changes to gamesApi file

--- client/src/store/apis/gamesApi.ts
+++ client/src/store/apis/gamesApi.ts
@@ -1,7 +1,5 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
-import dotenv from 'dotenv';
-dotenv.config();
-const apiKey = process.env.VITE_RAWG_API_KEY;
+const apiKey = import.meta.env.VITE_RAWG_API_KEY;
 const baseUrl = `https://api.rawg.io/api`;
 
 export interface GameType {
